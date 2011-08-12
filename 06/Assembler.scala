068a4ac923c011bc2e517a60b725261f07c27c33
Simon Morgan <sjm@spamcop.net>
2011-08-13 00:42:21 +0100

    Missed out the THAT register.

--- 06/Assembler.scala
+++ 06/Assembler.scala
@@ -161,7 +161,7 @@ object Code {
 
 class SymbolTable {
   var symbols = Map[String, Int](
-    "SP" -> 0x0000, "LCL" -> 0x0001, "ARG" -> 0x0002, "THIS" -> 0x0003,
+    "SP" -> 0x0000, "LCL" -> 0x0001, "ARG" -> 0x0002, "THIS" -> 0x0003, "THAT" -> 0x0004,
     "R0" -> 0x0000, "R1" -> 0x0001, "R2" -> 0x0002, "R3" -> 0x0003,
     "R4" -> 0x0004, "R5" -> 0x0005, "R6" -> 0x0006, "R7" -> 0x0007,
     "R8" -> 0x0008, "R9" -> 0x0009, "R10" -> 0x000A, "R11" -> 0x000B,
