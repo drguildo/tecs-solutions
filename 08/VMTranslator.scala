b6ba579c5fb189f7b1ffbb60ca0aac69714cf3b0
Simon Morgan <sjm@spamcop.net>
2011-10-01 21:31:18 +0100

    BasicLoop passes.

--- 08/VMTranslator.scala
+++ 08/VMTranslator.scala
@@ -1,19 +1,16 @@
 // Robustness is sacrificed for simplicity.
 
+import scala.collection.mutable.HashMap
+
 import java.io.File
 
 class Parser(file: File) {
-  import scala.io.Source
-  val code = Source.fromFile(file).getLines
-                                      .toList
-                                      .map(s => s.takeWhile(c => c != '/'))
-                                      .filter(s => s != "")
-                                      .map(s => s.split(' '))
-
-  val segmentRegisters = Map("local" -> "LCL", "argument" -> "ARG",
-                              "this" -> "THIS", "that" -> "THAT")
-
-  var labels = 0
+  val segmentRegisters = Map("local" -> "LCL",
+                             "argument" -> "ARG",
+                             "this" -> "THIS",
+                             "that" -> "THAT")
+  var labels: Set[String] = Set()
+  var currentFunction: Option[String] = None
 
   /*
   val stackBase = 256
@@ -50,19 +47,108 @@ class Parser(file: File) {
   println("M=D")
   */
 
-  for (command <- code) {
-    command(0) match {
-      case "push" => push(command(1), command(2).toInt)
-      case "pop"  => pop(command(1), command(2).toInt)
-      case "add"  => binaryOperation("M+D")
-      case "sub"  => binaryOperation("M-D")
-      case "eq"   => comparison("JEQ")
-      case "lt"   => comparison("JLT")
-      case "gt"   => comparison("JGT")
-      case "neg"  => neg()
-      case "and"  => binaryOperation("D&M")
-      case "or"   => binaryOperation("D|M")
-      case _      => throw new IllegalArgumentException("Invalid command.")
+  import scala.io.Source
+  var code = Source.fromFile(file).getLines.toList
+
+  for (line <- code) {
+    val command = line.split(' ')
+                      .map(_.trim)
+                      .filter(_ != "")
+                      .takeWhile(!_.contains('/')) // Remove comments.
+
+    if (command.length > 0) {
+      println("// " + command.mkString(" "))
+
+      command(0) match {
+        case "push" => push(command(1), command(2).toInt)
+        case "pop"  => pop(command(1), command(2).toInt)
+
+        case "add"  => binaryOperation("M+D")
+        case "sub"  => binaryOperation("M-D")
+
+        case "eq"   => comparison("JEQ")
+        case "lt"   => comparison("JLT")
+        case "gt"   => comparison("JGT")
+        case "neg"  => neg()
+        case "and"  => binaryOperation("D&M")
+        case "or"   => binaryOperation("D|M")
+
+        case "label"   => println("(" + expandLabel(command(1)) + ")")
+        case "goto"    => println("@" + expandLabel(command(1)))
+                          println("0;JMP")
+        case "if-goto" => decrementPointer("SP")
+                          dereferencePointer("SP")
+                          println("D=M")
+                          println("@" + expandLabel(command(1)))
+                          println("D;JNE")
+
+        case "function" => currentFunction = Some(command(1))
+        case "call"     => throw new Exception("Not implemented.")
+        case "return"   => currentFunction = None
+
+        case _ => throw new Exception("Invalid or unsupported command.")
+      }
+    }
+  }
+
+  // Memory access commands.
+
+  def push(segment: String, index: Int) {
+    // Fetch value from segment.
+    // XXX: Check segment index is valid.
+    if (segment == "constant") {
+      println("@" + index)
+      println("D=A")
+    } else if (segment == "static") {
+      println("@" + file.getName.stripSuffix(".vm") + "." + index)
+      println("D=M")
+    } else {
+      println("@" + index)
+      println("D=A")
+      segment match {
+        case "pointer" => println("@3")
+        case "temp"    => println("@5")
+        case _         => dereferencePointer(segmentRegisters(segment))
+      }
+      println("A=A+D")
+      println("D=M")
+    }
+
+    // Store value at the top of the stack.
+    // XXX: Check for stack overflow.
+    dereferencePointer("SP")
+    println("M=D")
+
+    // XXX: Check for stack underflow.
+    incrementPointer("SP")
+  }
+
+  def pop(segment: String, index: Int) {
+    if (segment == "static") {
+      decrementPointer("SP")
+      dereferencePointer("SP")
+      println("D=M")
+      println("@" + file.getName.stripSuffix(".vm") + "." + index)
+      println("M=D")
+    } else {
+      decrementPointer("SP")
+
+      println("@" + index)
+      println("D=A")
+      segment match {
+        case "pointer" => println("@3")
+        case "temp"    => println("@5")
+        case _         => dereferencePointer(segmentRegisters(segment))
+      }
+      println("D=A+D")
+      println("@R13")
+      println("M=D")
+
+      dereferencePointer("SP")
+      println("D=M")
+      println("@R13")
+      println("A=M")
+      println("M=D")
     }
   }
 
@@ -112,66 +198,13 @@ class Parser(file: File) {
     incrementPointer("SP")
   }
 
-  // Memory access commands.
+  // Program Flow
 
-  def push(segment: String, index: Int) {
-    // Fetch value from segment.
-    // XXX: Check segment index is valid.
-    if (segment == "constant") {
-      println("@" + index)
-      println("D=A")
-    } else if (segment == "static") {
-      println("@" + file.getName.stripSuffix(".vm") + "." + index)
-      println("D=M")
-    } else {
-      println("@" + index)
-      println("D=A")
-      segment match {
-        case "pointer" => println("@3")
-        case "temp"    => println("@5")
-        case _         => dereferencePointer(segmentRegisters(segment))
-      }
-      println("A=A+D")
-      println("D=M")
-    }
+  def label() {}
+  def goto() {}
+  def ifGoto() {}
 
-    // Store value at the top of the stack.
-    // XXX: Check for stack overflow.
-    dereferencePointer("SP")
-    println("M=D")
-
-    // XXX: Check for stack underflow.
-    incrementPointer("SP")
-  }
-
-  def pop(segment: String, index: Int) {
-    if (segment == "static") {
-      decrementPointer("SP")
-      dereferencePointer("SP")
-      println("D=M")
-      println("@" + file.getName.stripSuffix(".vm") + "." + index)
-      println("M=D")
-    } else {
-      decrementPointer("SP") // XXX: Move this just before dereferencePointer.
-
-      println("@" + index)
-      println("D=A")
-      segment match {
-        case "pointer" => println("@3")
-        case "temp"    => println("@5")
-        case _         => dereferencePointer(segmentRegisters(segment))
-      }
-      println("D=A+D")
-      println("@R13")
-      println("M=D")
-
-      dereferencePointer("SP")
-      println("D=M")
-      println("@R13")
-      println("A=M")
-      println("M=D")
-    }
-  }
+  // Function Calling
 
   // Helper functions.
 
@@ -192,9 +225,16 @@ class Parser(file: File) {
   }
 
   def generateLabel(): String = {
-    val l = "L" + labels
-    labels = labels + 1
+    val l = "L" + labels.size
+    labels = labels + l
     l
+  }
+
+  def expandLabel(label: String): String = {
+    currentFunction match {
+     case Some(n) => n + "$" + label
+     case None    => label
+    }
   }
 }
 
