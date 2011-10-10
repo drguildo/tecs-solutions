cda49cbc5b0149821fb7fa9b67b44619a6e32533
Simon Morgan <sjm@spamcop.net>
2011-10-10 22:17:34 +0100

    SimpleFunction passes and I also added not support which I somehow missed earlier. Oops.

--- 08/VMTranslator.scala
+++ 08/VMTranslator.scala
@@ -5,10 +5,12 @@ import scala.collection.mutable.HashMap
 import java.io.File
 
 class Parser(file: File) {
-  val segmentRegisters = Map("local" -> "LCL",
+  val filename = file.getName.stripSuffix(".vm")
+
+  val segmentRegisters = Map("local"    -> "LCL",
                              "argument" -> "ARG",
-                             "this" -> "THIS",
-                             "that" -> "THAT")
+                             "this"     -> "THIS",
+                             "that"     -> "THAT")
   var numLabels = 0
   var currentFunction: Option[String] = None
 
@@ -60,31 +62,39 @@ class Parser(file: File) {
       println("// " + command.mkString(" "))
 
       command(0) match {
+        // push segment index
         case "push" => push(command(1), command(2).toInt)
+        // pop segment index
         case "pop"  => pop(command(1), command(2).toInt)
 
         case "add"  => binaryOperation("M+D")
         case "sub"  => binaryOperation("M-D")
 
+        case "neg"  => unaryOperation("-M")
         case "eq"   => comparison("JEQ")
-        case "lt"   => comparison("JLT")
         case "gt"   => comparison("JGT")
-        case "neg"  => neg()
+        case "lt"   => comparison("JLT")
         case "and"  => binaryOperation("D&M")
         case "or"   => binaryOperation("D|M")
+        case "not"  => unaryOperation("!M")
 
+        // labal name
         case "label"   => println("(" + expandLabel(command(1)) + ")")
+        // goto name
         case "goto"    => println("@" + expandLabel(command(1)))
                           println("0;JMP")
+        // if-goto name
         case "if-goto" => decrementPointer("SP")
                           dereferencePointer("SP")
                           println("D=M")
                           println("@" + expandLabel(command(1)))
                           println("D;JNE")
 
-        case "function" => currentFunction = Some(command(1))
-        case "call"     => throw new Exception("Not implemented.")
-        case "return"   => currentFunction = None
+        // function name nlocals
+        case "function" => function(command(1), command(2).toInt)
+        // call name nargs
+        case "call"     => call(command(1), command(2).toInt)
+        case "return"   => return_()
 
         case _ => throw new Exception("Invalid or unsupported command.")
       }
@@ -100,7 +110,7 @@ class Parser(file: File) {
       println("@" + index)
       println("D=A")
     } else if (segment == "static") {
-      println("@" + file.getName.stripSuffix(".vm") + "." + index)
+      println("@" + filename + "." + index)
       println("D=M")
     } else {
       println("@" + index)
@@ -115,11 +125,10 @@ class Parser(file: File) {
     }
 
     // Store value at the top of the stack.
-    // XXX: Check for stack overflow.
     dereferencePointer("SP")
     println("M=D")
 
-    // XXX: Check for stack underflow.
+    // XXX: Check for stack overflow.
     incrementPointer("SP")
   }
 
@@ -128,7 +137,7 @@ class Parser(file: File) {
       decrementPointer("SP")
       dereferencePointer("SP")
       println("D=M")
-      println("@" + file.getName.stripSuffix(".vm") + "." + index)
+      println("@" + filename + "." + index)
       println("M=D")
     } else {
       decrementPointer("SP")
@@ -191,10 +200,10 @@ class Parser(file: File) {
     incrementPointer("SP")
   }
 
-  def neg() {
+  def unaryOperation(op: String) {
     decrementPointer("SP")
     dereferencePointer("SP")
-    println("M=-M")
+    println("M=" + op)
     incrementPointer("SP")
   }
 
@@ -204,7 +213,108 @@ class Parser(file: File) {
   def goto() {}
   def ifGoto() {}
 
-  // Function Calling
+  // Function calling
+
+  /* (f)
+   * repeat k times:
+   * push 0 */
+
+  def function(name: String, locals: Int) {
+    currentFunction = Some(name)
+    println("(" + name + ")")
+    for (i <- 0 until locals) {
+      dereferencePointer("SP")
+      println("M=0")
+      incrementPointer("SP")
+    }
+  }
+
+  /* push return-address
+   * push LCL
+   * push ARG
+   * push THIS
+   * push THAT
+   * ARG = SP-n-5
+   * LCL = SP
+   * goto f
+   * (return-address) */
+
+  def call(name: String, args: Int) {
+    val returnLabel = generateLabel()
+    for (symbol <- List(returnLabel, "LCL", "ARG", "THIS", "THAT")) {
+      println("@" + symbol)
+      println("D=A")
+      dereferencePointer("SP")
+      println("M=D")
+      incrementPointer("SP")
+    }
+
+    // ARG = SP-5-args
+    println("@" + (5+args))
+    println("D=A")
+    dereferencePointer("SP")
+    println("D=A-D")
+    println("@ARG")
+    println("M=D")
+
+    dereferencePointer("SP")
+    println("D=A")
+    println("@LCL")
+    println("M=D")
+
+    println("@" + name)
+    println("0;JMP")
+
+    println("(" + returnLabel + ")")
+  }
+
+  /* FRAME = LCL
+   * RET = *(FRAME-5)
+   * *ARG = pop()
+   * SP = ARG+1
+   * THAT = *(FRAME-1)
+   * THIS = *(FRAME-2)
+   * ARG = *(FRAME-3)
+   * LCL = *(FRAME-4)
+   * goto RET */
+
+  def return_() {
+    dereferencePointer("LCL")
+    println("D=A")
+    println("@FRAME")
+    println("M=D")
+
+    /* We reorder this thusly because it results in less code:
+     * 
+     * FRAME = LCL
+     * *ARG = pop()
+     * SP = ARG+1
+     * THAT = *(FRAME-1)
+     * THIS = *(FRAME-2)
+     * ARG = *(FRAME-3)
+     * LCL = *(FRAME-4)
+     * RET = *(FRAME-5)
+     * goto RET */
+
+    decrementPointer("SP")
+    dereferencePointer("SP")
+    println("D=M")
+    dereferencePointer("ARG")
+    println("M=D")
+
+    dereferencePointer("ARG")
+    println("D=A+1")
+    println("@SP")
+    println("M=D")
+
+    for (symbol <- List("THAT", "THIS", "ARG", "LCL", "RET")) {
+      decrementPointer("FRAME")
+      dereferencePointer("FRAME")
+      println("D=M")
+      println("@" + symbol)
+      println("M=D")
+    }
+  }
 
   // Helper functions.
 
@@ -226,6 +336,7 @@ class Parser(file: File) {
 
   def generateLabel(): String = {
     val l = "L" + numLabels
+    numLabels = numLabels + 1
     l
   }
 
